dc7ee773d1e53186a4e584b43dcd399c9675742c
Suhas Karanth <suhaskaranth2008@gmail.com>
2020-11-19 18:56:17 +0100

    Completed functions in train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,108 @@
+# import libraries
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+import re
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report, fbeta_score, make_scorer
+from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.model_selection import GridSearchCV
+import joblib
 
-
+# Function to load data
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('disaster_response', engine)
+    X = df['message']
+    y = df.iloc[:, 4:]
+    category_names = list(df.columns[4:])
+    return X, y, category_names
 
+# Create class to create starting verb
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
 
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+    
+# Create class to normalize the text
+class CaseNormalizer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        return pd.Series(X).apply(lambda x: x.lower()).values
+
+# Function to create clean tokens through text processing
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
 
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
 
+    return clean_tokens
+
+# Function to develop the model
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+            ('text_pipeline', Pipeline([
+                ('lowercase', CaseNormalizer()),
+                ('vect', CountVectorizer(tokenizer=tokenize,ngram_range= (1, 2))),
+                ('tfidf', TfidfTransformer())
+            ])),
+            ('starting_verb', StartingVerbExtractor())
+        ])),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators=200)))
+    ])
+    return pipeline
 
-
+# Function to evaluate the model and store the results to a dataframe
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    count = 0
+    results = pd.DataFrame(columns=['category', 'precision', 'recall', 'f_score'])
+    for category in category_names:
+        precision, recall, f_score, support = score(Y_test[category], y_pred[:,count], average='weighted')
+        results.at[count, 'category'] =category
+        results.at[count, 'precision'] = precision
+        results.at[count, 'recall'] = recall
+        results.at[count, 'f_score'] = f_score
+        count += 1
+    return results
 
-
+# Function to output the model for further use
 def save_model(model, model_filepath):
-    pass
+    joblib.dump(model, model_filepath)
 
 
 def main():
